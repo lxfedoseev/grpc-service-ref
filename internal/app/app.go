786b1f5018a53c44013f00e9b51efbc5a201cba7
Alexander Fedoseev/Russia RAlex.fedoseevD Lab Security Team Product Security Part <alex.fedoseev@lge.com>
2025-12-18 10:25:05 +0300

    first commit

---
+++ internal/app/app.go
@@ -0,0 +1,35 @@
+package app
+
+import (
+	"log/slog"
+	"time"
+
+	grpcapp "github.com/lxfedoseev/grpc-service-ref/internal/app/grpc"
+	"github.com/lxfedoseev/grpc-service-ref/internal/services/auth"
+	"github.com/lxfedoseev/grpc-service-ref/internal/storage/sqlite"
+)
+
+type App struct {
+	GRPCSrv *grpcapp.App
+}
+
+func New(
+	log *slog.Logger,
+	grpcPort int,
+	storagePath string,
+	tokenTTL time.Duration,
+) *App {
+
+	storage, err := sqlite.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	grpcApp := grpcapp.New(log, authService, grpcPort)
+
+	return &App{
+		GRPCSrv: grpcApp,
+	}
+}
